9d126b335ab3164bc98b1c8ff2913a3653052044
Albert Solernou <a.solernou@leeds.ac.uk>
2023-07-11 08:55:39 +0100

    [ uf ] tiny progress, moving to another machine.

--- src/uniformSampling.cpp
+++ src/uniformSampling.cpp
@@ -37,9 +37,22 @@ int main(int argc, char* argv[])
 
     std::vector<std::vector<scalar>> p;
     std::string line;
+    getline(fin, line); // ignore the one line header;
     while (getline(fin, line))
     {
         p.push_back(lineParser(line));
+    }
+
+    for (uint i = 1; i < p.size(); ++i)
+    {
+        arr2 po = {p[i-1][0], p[i-1][1]};
+        arr2 pi = {p[i][0],   p[i][1]};
+        arr2 t = mvf::tangent(po, pi);
+        if (!mvf::areCloseEnough(std::atan2(t[1], t[0]), p[i-1][3], 1e-6))
+        {
+            std::cout << "Err!" << std::endl;
+            return 1;
+        }
     }
 
     return 0;
