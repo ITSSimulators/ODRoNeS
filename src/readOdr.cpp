ITSSimulators/ODRoNeS

commit e865ce0a4447773aebb95071d2c968831ecec10c

[ readOdr ] better interface to use the different optimisations from Blender.

src/readOdr.cpp
@@ -517,10 +517,11 @@ void ReadOdr::simplifyGeometries(Odr::smaS &s, bool singleArc, bool straight, bo
         simplifyMultipleArcs(s);
 }
 
-void ReadOdr::simplifyGeometries(bool singleArc, bool straight, bool bezierToArcSeries)
+void ReadOdr::simplifyGeometries() // bool singleArc, bool straight, bool bezierToArcSeries)
 {
     for (uint i = 0; i < _sections.size(); ++i)
-        simplifyGeometries(_sections[i], singleArc, straight, bezierToArcSeries);
+        simplifyGeometries(_sections[i], _optimisations[i].singleArc,
+                           _optimisations[i].straightBits, _optimisations[i].arcSeries);
 
     return;
 }

include/readBOdr.h
@@ -39,6 +39,8 @@ public:
 
     void addConnection(Odr::udIndexed6DPoint &c);
 
+    void addSimplification(bool singleArc, bool straight, bool arcSeries);
+
 };
 
 } // namespace odrones;

include/readOdr.h
@@ -38,6 +38,13 @@ class ReadOdr
 {
 public:
     enum class kind { none, bodr, xodr };
+    struct optimisation
+    {
+        bool singleArc;
+        bool straightBits;
+        bool arcSeries;
+    };
+
     ReadOdr() : _k(kind::none) {};
     ReadOdr& operator=(const ReadOdr& r);
     ReadOdr& operator+=(const ReadOdr& r);
@@ -45,7 +52,7 @@ public:
     void renumber(uint shift); ///< renumber the Odr ids of the sections.
     void transform(const arr2 &position, scalar angle); ///< rotate and translate this map
 
-    void simplifyGeometries(bool singleArc, bool straight, bool bezierToArcSeries); ///< loop over every section simplify their geometries, using arcs and straights where possible.
+    void simplifyGeometries(); // bool singleArc, bool straight, bool bezierToArcSeries); ///< loop over every section simplify their geometries, using arcs and straights where possible.
 
     bool uniqueSectionOdrIDs(); ///< check whether there are sections with repeated odrIDs
 
@@ -89,6 +96,8 @@ protected:
     std::vector<Odr::smaS> _sections;
 
     std::vector<Odr::udIndexed6DPoint> _udConnections;
+
+    std::vector<optimisation> _optimisations;
 
     std::vector<Odr::speedRegulation> _defaultSpeedLimit;
 

src/readBOdr.cpp
@@ -33,5 +33,10 @@ void ReadBOdr::addConnection(Odr::udIndexed6DPoint &c)
     _udConnections.push_back(c);
 }
 
+void ReadBOdr::addSimplification(bool singleArc, bool straight, bool arcSeries)
+{
+    _optimisations.push_back({singleArc, straight, arcSeries});
+}
+
 
 

src/rns.cpp
@@ -368,10 +368,9 @@ bool RNS::makeOpenDRIVERoads(ReadOdr &read, const char* drivingSide, bool exhaus
     _ready = false;
 
     if (fineTune)
-    {
         fineTuneReadOdr(read);
-        read.simplifyGeometries(false, true, false);
-    }
+
+    read.simplifyGeometries();
     _letter = read;
 
     // Allocate and do the geometry for the lanes:

commit bc58b067690811f0ff6f7a8200886457ac0b587e

[ fix ] for an error introduced in e865ce0.

src/readOdr.cpp
@@ -519,6 +519,8 @@ void ReadOdr::simplifyGeometries(Odr::smaS &s, bool singleArc, bool straight, bo
 
 void ReadOdr::simplifyGeometries() // bool singleArc, bool straight, bool bezierToArcSeries)
 {
+    if (_sections.size() != _optimisations.size()) return;
+
     for (uint i = 0; i < _sections.size(); ++i)
         simplifyGeometries(_sections[i], _optimisations[i].singleArc,
                            _optimisations[i].straightBits, _optimisations[i].arcSeries);